209ae6216c7601b87cdbceaf5df48c7b355c2161
Stanislav Okunev <stanok3694@yandex.ru>
2018-03-02 01:44:14 +0300

    deploy: added invalid date catch

--- src/utils/FormatDate.js
+++ src/utils/FormatDate.js
@@ -1,16 +1,12 @@
 import moment from "moment";
 
 const formatDate = (unformattedDate) => {
-    console.log('i am used - formatDate');
     const result = moment(unformattedDate, 'DD-MM-YYYY').format('L');
-    console.log(`formatDate - ${result}`);
     return result;
 }
 
 const formatDateForUI = (unformattedDate) => {
-    console.log('i am used - formatDateForUI');
     const result = moment(unformattedDate).format("DD.MM.YYYY");
-    console.log(`formatDateForUI - ${result}`);
     return result;
 }
 
